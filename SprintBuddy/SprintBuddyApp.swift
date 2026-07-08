499adf6647ea16340e052d3f25527edef77929be
Rajesh Hadiya <hadiarajesh007@gmail.com>
2026-07-08 22:27:38 +0530

    Initial Commit

---
+++ SprintBuddy/SprintBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SprintBuddyApp.swift
+//  SprintBuddy
+//
+//  Created by Rajesh Hadiya on 08/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SprintBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
